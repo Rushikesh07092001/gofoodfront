712703e4e4d9602cd8eb7ec21506edec68fe37dd
rushikesh <rushikeshchopde8086@gmail.com>
2023-06-16 23:28:20 +0530

    second commit

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const mongoDB = require("./db")
 const path = require('path')
     // const path = require('path')
 
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     next();
-// });
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://gofoodapp-74da.onrender.com");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 
 
